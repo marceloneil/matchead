d45d2b82af59e68b7a0fa1848c2cb98ce1ef32dd
Kevin S <kshen3778@gmail.com>
2015-08-22 14:21:09 +0000

    Added Angular services for post functionality

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var app = angular.module('flapperNews', []);
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
 app.controller('MainCtrl', [
     '$scope',
-    function($scope){
-        $scope.posts = [
-          {title: 'post 1', upvotes: 5},
-          {title: 'post 2', upvotes: 2},
-          {title: 'post 3', upvotes: 15},
-          {title: 'post 4', upvotes: 9},
-          {title: 'post 5', upvotes: 4}
-        ];
+    'posts',
+    function($scope, posts){
+        $scope.posts = posts.posts;
         
         //add a new post (link is optional)
         $scope.addPost = function(){
