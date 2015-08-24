e5fe4ee6370a0f0686b282def9e8fd1c07c623e3
Kevin S <kshen3778@gmail.com>
2015-08-24 14:57:10 +0000

    Added commenting

--- app.js
+++ app.js
@@ -19,7 +19,12 @@ app.controller('MainCtrl', [
               return;
           }
           
-          $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});  
+          $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0,
+            comments: [
+              {author: 'Joe', body: 'Cool post!', upvotes: 0},
+              {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+            ]
+          });  
           $scope.title = "";
           $scope.link = "";
         };
@@ -31,15 +36,33 @@ app.controller('MainCtrl', [
     }
 ]);
 
+//will control posts and comments
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){ //has acces to posts factory
+    $scope.posts = posts.posts[$stateParams.id];
+    
+}]);
+
 app.config([
 '$stateProvider',
 '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider){
     
+    //home state
     $stateProvider.state('home', {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    });
+    
+    //posts and comments
+    $stateProvider.state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
     
     $urlRouterProvider.otherwise('home');
