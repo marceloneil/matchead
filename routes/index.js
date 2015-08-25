a96c3fcbf04b5ce0bcb6880f26c5d42124d4e2ad
Kevin S <kshen3778@gmail.com>
2015-08-25 18:46:17 +0000

    Set and tested routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+//Note: Use this url for curl instead of localhost:3000: https://mean-news-app-kshen3778.c9.io/
+//GET all posts
+router.get('/posts', function(req,res,next){
+    //retrieve all posts in JSON format
+   Post.find(function(err, posts){
+     if(err){ return next(err); }
+     
+     res.json(posts);
+   });
+});
+
+//POST create a post
+router.post('/posts', function(req, res, next){
+   var post = new Post(req.body); //create a new post with user input info
+   
+   post.save(function(err, post){
+      if(err){ return next(err); } 
+      
+      res.json(post);
+   });
+   
+});
+
+
 module.exports = router;
+
