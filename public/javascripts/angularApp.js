1ccdcb5cf2f67424c4d913ff69577b4ba30f0902
Kevin S <kshen3778@gmail.com>
2015-08-31 17:06:41 +0000

    set up angular authentication

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -41,6 +41,60 @@ app.factory('posts', ['$http', function($http){
   return o;
 }]);
 
+app.factory('auth', ['$http', '$window', function($http, $window){
+  var auth = {};
+  
+  //save the login token into localStorage
+  auth.saveToken = function(token){
+    $window.localStorage['flapper-news-token'] = token;
+  };
+  
+  //get token from localStorage
+  auth.getToken = function(){
+    return $window.localStorage['flapper-news-token'];
+  };
+  
+  //check if user is logged in(token exists and isn't expired)
+  auth.isLoggedIn = function(){
+    var token = auth.getToken();
+    if(token){ //check if token exists
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+      return payload.exp > Date.now() / 1000; //check for expiration
+    } else {
+      return false; //user is logged out
+    }
+  };
+  
+  //return username of user that's logged in
+  auth.currentUser = function(){
+    if(auth.isLoggedIn()){
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+      return payload.username;
+    }
+  };
+  
+  //register the user and save the token returned
+  auth.register = function(user){
+    return $http.post('/register', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+  
+  //login the user and save the token returned
+  auth.logIn = function(user){
+    return $http.post('/login', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+  
+  //logout by removing token from localStorage
+  auth.logOut = function(){
+    $window.localStorage.removeItem('flapper-news-token');
+  };
+  return auth;
+}]);
+
 //controls posts
 app.controller('MainCtrl', [
     '$scope',
@@ -98,6 +152,32 @@ function($scope, posts, post){ //has access to posts factory
       posts.upvoteComment(post, comment);
     };
     
+}]);
+
+app.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+  
+  //calls the register method in auth factory
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){ //if no errors, promise the user home
+      $state.go('home');
+    });
+  };
+  
+  //calls the login method in auth factory
+  $scope.login = function(){
+    auth.login($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){ //if no errors, promise the user home
+      $state.go('home');
+    });
+  };
 }]);
 
 app.config([
