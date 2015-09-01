ad9c2c4c7f9652bcfd203d607bb8e17632781f04
Kevin S <kshen3778@gmail.com>
2015-09-01 19:18:06 +0000

    Cleared database

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ var passport = require('passport');
 
 //Connect to MongoDB Mongoose instance
 var mongoose = require("mongoose");
-//require('./models/Posts');
-//require('./models/Comments');
 //mongoose.connect("mongodb://kshen3778@gmail.com:qazwsx1703@" + process.env.IP + ":" + process.env.PORT + "/news");
+//to delete everything in the 'news' database: mongo kshen3778-mean-news-app-1800429:27017/news  --eval "db.dropDatabase();"
 mongoose.connect("mongodb://kshen3778-mean-news-app-1800429:27017/news");
 require('./models/Posts');
 require('./models/Comments');
